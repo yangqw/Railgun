de47549a1ad621a1204f7507ac13ca2604324170
Kevin Qiwei Yang <yangqw@shinetechsoftware.com>
2026-03-11 11:32:18 +0800

    Update PassWall repository links in script
    Updated PassWall repository links to the new Openwrt-Passwall organization.

--- diy/ramips_mt7621/phicomm_k2p/diy-before-feeds.sh
+++ diy/ramips_mt7621/phicomm_k2p/diy-before-feeds.sh
@@ -12,6 +12,8 @@ function git_sparse_clone() {
 # Enable helloworld
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add passwall
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default    
+# Add passwall，PassWall 的维护仓库已从 xiaorouji 个人名下彻底迁移到了 Openwrt-Passwall 组织名下
+# echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
+echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >> feeds.conf.default
+echo 'src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' >> feeds.conf.default
