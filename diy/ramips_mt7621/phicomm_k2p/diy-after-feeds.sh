d7fc43d85ba88975421d759c3f8e70028e94ab39
Kevin Qiwei Yang <yangqw@shinetechsoftware.com>
2026-03-11 16:13:40 +0800

    Modify Xray compression and SquashFS block size
    Updated Xray core compression and SquashFS block size.

--- diy/ramips_mt7621/phicomm_k2p/diy-after-feeds.sh
+++ diy/ramips_mt7621/phicomm_k2p/diy-after-feeds.sh
@@ -9,3 +9,12 @@ sed -i 's/OpenWrt/Railgun/g' package/base-files/files/bin/config_generate
 # Enable 802.11 r
 sed -i 's/FtSupport=0/FtSupport=1/g' package/lean/mt/drivers/mt_wifi/files/mt7615.1.2G.dat
 sed -i 's/FtSupport=0/FtSupport=1/g' package/lean/mt/drivers/mt_wifi/files/mt7615.1.5G.dat
+
+
+# 1. 强制对 Xray 核心进行极致压缩 (--ultra-brute)
+# 注意：这会显著增加 GitHub Actions 的编译时间，但能省下约 1MB 空间
+find feeds/passwall/ -name "Makefile" | xargs sed -i 's/$(1)\/usr\/bin\/xray/$(STAGING_DIR_HOST)\/bin\/upx --ultra-brute $(1)\/usr\/bin\/xray/g'
+
+# 2. 修改 SquashFS 块大小（从 256k 改为 1024k）
+# 这会让固件只读层的压缩率更高，通常能再省下 200-300KB
+sed -i 's/256k/1024k/g' target/linux/ramips/image/mt7621.mk
